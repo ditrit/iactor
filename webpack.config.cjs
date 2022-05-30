9605b3cc3a38f958326131ea0c42cebbfb8b5835
NolwennMiossec <nolwenn.miossec@free.fr>
2022-05-30 09:18:01 +0200

    Add a webpack config

---
+++ webpack.config.cjs
@@ -0,0 +1,55 @@
+const path = require("path");
+
+module.exports = {
+	mode: "production",
+	entry: {
+		bundle: path.resolve(__dirname, './src/plugins/terraform/index.js'),
+	},
+	output: {
+		path: path.resolve(__dirname, "../frontend/public/dist"),
+		filename: "[name].js",
+		clean: true,
+	},
+	optimization: {
+		splitChunks: {
+			// chunks: "all",
+			cacheGroups: {
+				defaultVendors: {
+					test: /[\\/]node_modules[\\/]/,
+					priority: -10,
+					reuseExistingChunk: true,
+				},
+				default: {
+					minChunks: 2,
+					priority: -20,
+					reuseExistingChunk: true,
+				},
+			},
+		},
+	},
+	performance: {
+		maxEntrypointSize: 400000,
+		hints: "warning",
+	},
+	devtool: "source-map",
+	resolve: {
+		fallback: {
+			fs: false,
+		},
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+		],
+	}
+};
