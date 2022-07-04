b07a4e7cc06bfd07837bd0b88e15d33631eea764
Vincent Moittié <vincent.moittie@cji.paris>
2022-07-04 14:05:08 +0200

    Delete all src files

--- src/listener/name.js
+++
@@ -1,8 +0,0 @@
-import { newTerraformName } from '../model/name.js';
-
-export default {
-  exit_name(parsed_rule) {
-    const name = (parsed_rule.ctx.ctx) ? parsed_rule.ctx.ctx.getText().replaceAll('"', '') : '';
-    parsed_rule.prog.current_file.name = newTerraformName(name, parsed_rule.ctx);
-  },
-};
